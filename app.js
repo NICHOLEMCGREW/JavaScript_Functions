2451fed97ce3c72e1d981cd319e6f097a8f164e7
Nichole McGrew <nicholemcgrew@gmail.com>
2022-01-13 19:30:58 -0600

    commit functions exercise

--- app.js
+++ app.js
@@ -3,5 +3,31 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+function printOdds(count) {
+    for (let i = 1; i <= count; i++) {
+        if(i % 2 == 1) {
+        console.log(i); 
+        }  
+    }
+}
+
+
+printOdds(81);
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+function checkAge(name, age) {
+    let aboveSixteen = `Congrats ${name}, you can drive!`;
+    let belowSixteen = `Sorry ${name}, but you need to wait until you are 16.`;
+
+    if (age < 16) {
+        console.log(belowSixteen); 
+    } else {
+       console.log(aboveSixteen); 
+    }    
+}
+
+checkAge("John", 34);
+checkAge("Joe", 14);
+checkAge("Jane", 23);
